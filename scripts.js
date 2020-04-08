279ed0685070fe7e7937340ee43a44befb789068
Akash <gsaisriram.akash@gmail.com>
2020-04-08 23:07:57 +0530

    scrollreveal.js animations added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+
+
+/* Preloader */
+
+
+
+
+
+/* Carousel */
+
 var slideIndex = 2;
 showSlides(slideIndex);
 
@@ -18,5 +28,55 @@ function showSlides(n) {
       slides[i].style.display = "none";  
   }
   
-  slides[slideIndex-1].style.display = "block";    
+  slides[slideIndex-1].style.display = "block";  
 }
+
+/* Animations using scrollreveal */
+
+
+
+window.sr = ScrollReveal();
+
+
+sr.reveal('.nav-container', {
+  duration:4500,
+  origin:'bottom'
+});
+
+
+sr.reveal('.home-image', {
+    duration:3000,
+    origin:'top',
+    distance:'200px'
+});
+
+sr.reveal('.home-wrap', {
+    duration:3000,
+    origin:'left',
+    distance:'300px'
+});
+
+sr.reveal('.points-container', {
+    duration:4000,
+    origin:'right',
+    distance:'300px'
+});
+
+sr.reveal('.features-image', {
+    duration:3000,
+    origin:'top',
+    distance:'300px'
+});
+
+sr.reveal('.pics-container', {
+    duration:3400,
+    origin:'bottom',
+    distance:'400px'
+})
+
+sr.reveal('.reviews', {
+  duration:3400,
+  origin:'bottom',
+  distance:'400px'
+})
+
